c9111864ad06ece6694000123cb2980c0b107abd
Dobeen Kim <dobin7955@gmail.com>
2026-04-08 17:44:55 +0200

    add logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,14 @@
+import {NavLink} from 'react-router-dom'
 import styles from './header.module.css'
 import Menu from '../../components/Menu'
+import logo from '../../assets/movie-logo.png'
 
 
 const Header = () => {
     return (
         <header>
-            <h2 className={styles.text}>For You</h2>
+            <NavLink to={`/`} className={styles.logo}><img src={logo} alt="logo" /></NavLink>
+            <p className={styles.text}>For You</p>
             <Menu />
         </header>
     )
